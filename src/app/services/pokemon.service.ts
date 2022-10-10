5ae3496a42c3608d40d2e0841e0267504b1257c2
Nadilson <nadilson@protonmail.com>
2022-10-10 05:40:29 -0300

    Adicionando módulo HTTP

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,5 +1,6 @@
 import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +8,14 @@ import { Injectable } from '@angular/core';
 export class PokemonService {
 
   private baseURL: string = "";
+  private pokeData: any;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.baseURL = environment.pokeApi;
   }
 
   getPokemon(pokemonName: string) {
-    console.log(this.baseURL);
+    this.pokeData = this.http.get(`${this.baseURL}/${pokemonName}`);
+    console.log(this.pokeData);
   }
 }
