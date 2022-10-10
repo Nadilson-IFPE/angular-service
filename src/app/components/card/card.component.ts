cc57aa206f573a4aa052a7211533aa031a8cf9b5
Nadilson <nadilson@protonmail.com>
2022-10-10 06:57:36 -0300

    Consumo de API finalizado

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -10,7 +10,7 @@ import { PokemonData } from 'src/app/models/pokemonData';
 export class CardComponent implements OnInit {
   pokemon: PokemonData;
   name: string = "CHARIZARD";
-  attributesTypes: string[] = ['FIRE', 'ROCK'];
+  //attributesTypes: string[] = ['FIRE', 'ROCK'];
 
   constructor(private service: PokemonService ) {
 
@@ -26,7 +26,16 @@ export class CardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.service.getPokemon("pikachu").subscribe({
+    this.getPokemon("pikachu");
+  }
+
+
+  getPokemon(searchName: string) {
+    if (searchName === "") {
+      searchName = "pikachu";
+    }
+
+    this.service.getPokemon(searchName).subscribe({
       next: (res) => {
 
         this.pokemon = {
@@ -39,7 +48,7 @@ export class CardComponent implements OnInit {
         // console.log(res.types);
         // console.log(this.pokemon);
       },
-      error: (err) => console.log(err)
+      error: (err) => console.log("not found")
     });
   }
 
